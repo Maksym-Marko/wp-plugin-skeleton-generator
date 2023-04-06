75f50f7ae7effa052ba092a0c3902cefd3dc12a2
Maksym Marko <markomaksym@gmail.com>
2023-04-06 12:28:00 +0300

    Added several properties to Model class. Fixed "default value" problem for longtext columns for olde versions of MySQL.

--- generate/input/includes/core/Model.php
+++ generate/input/includes/core/Model.php
@@ -36,14 +36,14 @@ class |UNIQUESTRING|Model
     /**
     * select row from the database
     */
-    public function getRow( $table = NULL, $wherName = NULL, $wherValue = NULL )
+    public function getRow( $table = NULL, $wherName = NULL, $wherValue = NULL, $and = '' )
     {
 
         $tableName = $this->wpdb->prefix . $this->table;
 
         if ($table !== NULL) {
 
-            $tableName = $table;
+            $tableName = $this->wpdb->prefix . $table;
 
         }
 
@@ -55,7 +55,7 @@ class |UNIQUESTRING|Model
 
         }
 
-        $getRow = $this->wpdb->get_row( "SELECT $this->fields FROM $tableName {$where}" );
+        $getRow = $this->wpdb->get_row( "SELECT $this->fields FROM $tableName {$where} {$and}" );
 
         return $getRow;
         
@@ -64,12 +64,12 @@ class |UNIQUESTRING|Model
     /**
     * get results from the database
     */
-    public function getResults( $table = false, $wherName = NULL, $wherValue = 1 )
+    public function getResults( $table = NULL, $wherName = NULL, $wherValue = 1, $and = '', $order = 'ORDER BY id DESC', $mask = '%d' )
     {
 
         $tableName = $this->wpdb->prefix . $this->table;
 
-        if ($table !== false) {
+        if ($table !== NULL) {
 
             $tableName = $table;
 
@@ -77,7 +77,19 @@ class |UNIQUESTRING|Model
 
         if ($wherName !== NULL) {
 
-            $results = $this->wpdb->get_results( "SELECT $this->fields FROM $tableName WHERE $wherName = $wherValue" );
+            $results = $this->wpdb->get_results( 
+
+                $this->wpdb->prepare(
+
+                    "SELECT $this->fields
+                        FROM $tableName
+                        WHERE $wherName=$mask {$and}
+                        {$order}",
+                    $wherValue
+
+                )
+
+            );
 
         } else {
 
@@ -87,6 +99,82 @@ class |UNIQUESTRING|Model
 
         return $results;
         
+    }
+
+    /**
+    * update row
+    */
+    public function updateRow( $table = NULL, $wherName = NULL, $wherValue = NULL, $columns = [], $masks = [] ){
+
+        $tableName = $this->wpdb->prefix . $this->table;
+
+        if ($table !== NULL) {
+
+            $tableName = $this->wpdb->prefix . $table;
+
+        }
+
+        if($wherName == NULL || $wherValue == NULL) return false;
+
+        $update = $this->wpdb->update(
+
+            $tableName,
+            $columns,
+            [
+                $wherName => $wherValue
+            ],
+            $masks
+
+        );
+
+        return $update;
+
+    }
+
+    /**
+    * insert row
+    */
+    public function insertRow( $table = NULL, $columns = [], $masks = [] ) {
+
+        $tableName = $this->wpdb->prefix . $this->table;
+
+        if ($table !== NULL) {
+
+            $tableName = $this->wpdb->prefix . $table;
+
+        }
+
+        $insert = $this->wpdb->insert(
+
+            $tableName,
+
+            $columns,
+
+            $masks
+
+        );
+
+        return $insert;
+
+    }
+
+    /**
+     * get var
+     */
+    public function getVar($table = NULL, $countBy = 'id', $and = null)
+    {
+
+        $tableName = $this->wpdb->prefix . $this->table;
+
+        if ($table !== NULL) {
+
+            $tableName = $this->wpdb->prefix . $table;
+        }
+
+        $count = $this->wpdb->get_var( "SELECT COUNT($countBy) FROM {$tableName} WHERE 1=1 {$and}");
+
+        return $count;
+
     }
 
 }
--- generate/input/includes/core/create-table.php
+++ generate/input/includes/core/create-table.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) exit;
@@ -11,7 +11,7 @@ class |UNIQUESTRING|CreateTable
 
     // columns
     private $columns       = [];
- 
+
     // SQL query
     private $sqlContainer = NULL;
 
@@ -21,73 +21,73 @@ class |UNIQUESTRING|CreateTable
     // datetime
     private $datetime      = NULL;
 
-    function __construct( $tableName = 'mx_table' )
+    function __construct($tableName = 'mx_table')
     {
 
         global $wpdb;
 
         $this->datetime = current_time('mysql');
 
-        $this->wpdb     = $wpdb; 
+        $this->wpdb     = $wpdb;
 
         $this->table    = $tableName;
-
     }
 
     // add varchar
-    public function varchar( $columnName = 'name', $length = 10, $notNull = false, $default = NULL )
+    public function varchar($columnName = 'name', $length = 10, $notNull = false, $default = NULL)
     {
 
         // not null
         $notNull = $notNull ? 'NOT NULL' : 'NULL';
 
         // default
-        $default = $default !== NULL ? 'default \'' . $default .'\'' : '';
+        $default = $default !== NULL ? 'default \'' . $default . '\'' : '';
 
         $sql     = "$columnName varchar($length) $notNull $default";
 
-        array_push( $this->columns, $sql );
-
+        array_push($this->columns, $sql);
     }
 
     // add longtext
-    public function longtext( $columnName = 'text', $notNull = false )
+    public function longtext($columnName = 'text', $notNull = false)
     {
 
         // not null
         $notNull = $notNull ? 'NOT NULL' : 'NULL';
 
+        /**
+         *  "default" doesn't work for old MySQL versions
+         * */
+        // $default = $default !== NULL ? 'default \'' . $default . '\'' : '';
+
         $sql     = "$columnName longtext $notNull";
 
-        array_push( $this->columns, $sql );
-
+        array_push($this->columns, $sql);
     }
 
     // add int
-    public function int( $columnName = 'integer' )
+    public function int($columnName = 'integer')
     {
 
         $sql = "$columnName int(11) NOT NULL";
 
-        array_push( $this->columns, $sql );
-
+        array_push($this->columns, $sql);
     }
 
     // add datetime
-    public function datetime( $columnName = 'created', $default = NULL )
+    public function datetime($columnName = 'created', $default = NULL)
     {
 
         // default
-        $default = $default == NULL ? current_time('mysql') : $default;
+        $default = $default == NULL ? '0000-00-00 00:00:00' : $default;
 
         $sql     = "$columnName datetime NOT NULL default '$default'";
 
-        array_push( $this->columns, $sql );
-
+        array_push($this->columns, $sql);
     }
 
     // we should to add some coluns to the table
-    public function create_columns( $id = 'id' )
+    public function create_columns($id = 'id')
     {
 
         global $wpdb;
@@ -97,21 +97,19 @@ class |UNIQUESTRING|CreateTable
         if ($wpdb->has_cap('collation')) {
 
             $collate = $wpdb->get_charset_collate();
-
         }
 
         // get all columns
-        $columns = implode( ',', $this->columns );
+        $columns = implode(',', $this->columns);
 
         // create a table
-        if (count( $this->columns) == 0) {
+        if (count($this->columns) == 0) {
 
             $this->sqlContainer = "CREATE TABLE IF NOT EXISTS `$this->table`
                 (
                     `$id` int(11) NOT NULL AUTO_INCREMENT,
                     PRIMARY KEY (`$id`)
                 ) $collate;";
-
         } else {
 
             $this->sqlContainer = "CREATE TABLE IF NOT EXISTS `$this->table`
@@ -120,10 +118,7 @@ class |UNIQUESTRING|CreateTable
                     $columns,
                     PRIMARY KEY (`$id`)
                 ) $collate;";
-
         }
-
-
     }
 
     public function createTable()
@@ -135,15 +130,12 @@ class |UNIQUESTRING|CreateTable
         if ($this->wpdb->get_var("SHOW TABLES LIKE '" . $this->table . "'") != $this->table) {
 
             // create a table
-            $this->wpdb->query( $this->sqlContainer );
+            $this->wpdb->query($this->sqlContainer);
 
             return 1;
-
         } else {
 
             return 0;
-
         }
-
     }
 }
