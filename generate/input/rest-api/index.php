aa3be7e3dbcdc32fa98ea8f4f3fef9e8be277056
Maksym Marko <markomaksym@gmail.com>
2024-01-28 16:45:55 +0200

    Vue. WP styles added.

---
+++ generate/input/rest-api/index.php
@@ -0,0 +1,92 @@
+<?php
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) exit;
+
+/**
+ * The REST API main file.
+ *
+ * Here you can register your REST API endpoints.
+ */
+
+if (!function_exists('mxpfwra_wpp_generator_get_endpoint_pages')) {
+    /**
+     * Return all the pages with status publish.
+     * 
+     * @return void
+     */
+    function mxpfwra_wpp_generator_get_endpoint_pages()
+    {
+
+        $pages = get_pages([
+            'post_status' => 'publish'
+        ]);
+
+        if (is_array($pages)) {
+
+            foreach ($pages as $key => $value) {
+
+                $content = apply_filters('the_content', $value->post_content);
+
+                $pages[$key]->post_content = $content;
+            }
+        }
+
+        return rest_ensure_response($pages);
+    }
+}
+
+if (!function_exists('mxpfwra_wpp_generator_get_endpoint_navigation')) {
+    /**
+     * Return navigation.
+     * 
+     * @return void
+     */
+    function mxpfwra_wpp_generator_get_endpoint_navigation()
+    {
+
+        $navigation = get_posts([
+            'post_type' => 'wp_navigation'
+        ]);
+
+        if (is_array($navigation)) {
+
+            foreach ($navigation as $key => $value) {
+
+                $content = apply_filters('the_content', $value->post_content);
+
+                $navigation[$key]->post_content = $content;
+            }
+        }
+
+        return rest_ensure_response($navigation);
+    }
+}
+
+if (!function_exists('mxpfwra_wpp_generator_register_example_routes')) {
+    /**
+     * Here you can register your routes
+     * And example: http://gutenberg.local/wp-json/wpp-generator/v1/pages
+     * @return void
+     */
+    function mxpfwra_wpp_generator_register_example_routes()
+    {
+
+        /**
+         * Get navigation.
+         */
+        register_rest_route('wpp-generator/v1', '/navigation', array(
+            'methods'  => WP_REST_Server::READABLE,
+            'callback' => 'mxpfwra_wpp_generator_get_endpoint_navigation',
+        ));
+
+        /**
+         * Get pages.
+         */
+        register_rest_route('wpp-generator/v1', '/pages', array(
+            'methods'  => WP_REST_Server::READABLE,
+            'callback' => 'mxpfwra_wpp_generator_get_endpoint_pages',
+        ));
+    }
+}
+add_action('rest_api_init', 'mxpfwra_wpp_generator_register_example_routes');
